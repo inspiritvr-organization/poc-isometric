648530f3ff6294c48465d1f84f80094341ec32c7
Carlos Felipe <carlos@cheerioworld.com>
2022-03-30 15:28:48 -0500

    Added model viewer scene

--- Assets/Dependencies/PocMetaverseIsometric/PocMetaverseIsometric_Scripts/GameManager.cs
+++ Assets/Dependencies/PocMetaverseIsometric/PocMetaverseIsometric_Scripts/GameManager.cs
@@ -8,9 +8,9 @@ public class GameManager : MonoBehaviour
     public Action<InteractionObject> OnTriggerActor;
     public IsometricCharacterController character;
 
-   [SerializeField] private InputManager inputManager;
-   [SerializeField] private UIHandler uIHandler;
-   
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private UIHandler uIHandler;
+
     public static GameManager Instance
     {
         get
@@ -19,7 +19,7 @@ public class GameManager : MonoBehaviour
         }
         set
         {
-            if(instance==null)
+            if (instance == null)
             {
                 instance = value;
             }
@@ -40,7 +40,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if(Instance==null)
+        if (Instance == null)
         {
             Instance = this;
         }
---
+++ Assets/Dependencies/PocMetaverseIsometric/PocMetaverseIsometric_Scripts/Input/ModelViewerInput.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ModelViewerInput : MonoBehaviour, IBeginDragHandler, IDragHandler
+{
+    private Vector3 touchStart;
+    private Vector2 mouseReference;
+    [SerializeField] private float zoomOutMin = 1;
+    [SerializeField] private float zoomOutMax = 8;
+    [SerializeField] private float rotationSpeed = 0.5f;
+
+    [SerializeField] private Transform modelBase;
+
+    private void OnEnable()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        WebGLInput.captureAllKeyboardInput = true;
+#endif
+    }
+    private void OnDisable()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        WebGLInput.captureAllKeyboardInput = false;
+#endif
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            Zoom(difference * 0.01f);
+        }
+
+        Zoom(Input.GetAxis("Mouse ScrollWheel"));
+    }
+
+    private void Zoom(float increment)
+    {
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
+    }
+
+    private void Rotate(Vector2 deltaRotation)
+    {
+        modelBase.rotation = Quaternion.Euler(Mathf.Clamp(modelBase.rotation.y - deltaRotation.y, -90, 90), Mathf.Clamp(modelBase.rotation.x + deltaRotation.x, -180, 180), 0);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Rotate((mouseReference - eventData.position) * rotationSpeed);
+
+        eventData.position = mouseReference;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        mouseReference = eventData.position;
+    }
+}
